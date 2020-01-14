3dd436adfe628f58d63d4e97b1d8aa2aca1e77b6
cocofrut <jackiewicz.raf@gmail.com>
2020-01-14 10:59:28 +0000

    Initial commit

---
+++ src/main/java/eu/felixfabrica/ocr/OcrApplication.java
@@ -0,0 +1,13 @@
+package eu.felixfabrica.ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrApplication.class, args);
+    }
+
+}
